fdbec47d8b07efa4ea5d1c4a9d5fc02a8b46daf3
Oleksandr Dorobalo <al.dorobalo@gmail.com>
2024-09-25 23:26:28 +0200

    timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -77,3 +77,77 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 //
+
+//timer
+// Устанавливаем дату, до которой будет обратный отсчет
+const targetDate = new Date('October 26, 2024 18:30:00').getTime();
+
+// Обновляем таймер каждую секунду
+const countdown = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  // Вычисляем дни, часы, минуты и секунды
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Отображаем результат в элементе с id="countdown"
+  document.getElementById('countdown').innerHTML =
+    days + " д " + hours + " ч " + minutes + " м " + seconds + " с ";
+
+  // Если обратный отсчет завершился, выводим сообщение
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById('countdown').innerHTML = "Таймер завершен!";
+  }
+}, 1000);
+//
+
+
+//
+
+const year = new Date().getFullYear();
+const fourthOfJuly = new Date(year, 6, 4).getTime();
+const fourthOfJulyNextYear = new Date(year + 1, 6, 4).getTime();
+const month = new Date().getMonth();
+
+// countdown
+let timer = setInterval(function () {
+
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  let diff;
+  if (month > 6) {
+    diff = fourthOfJulyNextYear - today;
+  } else {
+    diff = fourthOfJuly - today;
+  }
+
+
+
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("timer").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
+
+//
